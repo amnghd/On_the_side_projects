0caa668dad1b5934c31c1880384e54c48eefee59
amnghd <ghaderi.amin@gmail.com>
2019-03-30 09:37:48 -0700

    some minor modifications

---
+++ Some learning material/mycleancode.py
@@ -0,0 +1,10 @@
+# this is a python magic function writing the cell content into a file
+
+# let's write a not easy to read piece of code
+thisISaDictionary = {1: 'one', 2: 'two', 3: 'three'}
+# this is one comment
+
+# and here is an arithmentic operation
+a = 2
+b = 3
+c = a - b
---
+++ Some learning material/myflunkycode.py
@@ -0,0 +1,10 @@
+# let's write a not easy to read piece of code
+thisISaDictionary = {1:'one',
+    2:'two',
+                    3:'three'}
+### this is one comment
+
+# and here is an arithmentic operation
+a=2
+b=3
+c=a-b
